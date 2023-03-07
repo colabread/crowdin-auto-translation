423540dea85a77515fdc432c072c59541cee528e
唐佳 <game428@users.noreply.github.com>
2023-03-07 11:34:43 +0800

    feat: add window zip

--- src/unzip.js
+++ src/unzip.js
@@ -1,13 +1,24 @@
-const { exec } = require('child_process');
+const { exec } = require("child_process");
+const os = require("os");
+const adm_zip = require("adm-zip");
 
 const unzip = (outputAbsolutePath) => {
   return new Promise((resolve, reject) => {
-    const cmd = `unzip ${outputAbsolutePath}/translated.zip -d ${outputAbsolutePath}/translated`;
-    exec(cmd, (err, stdout, stderr) => {
-      if (err) reject(stderr);
-      else resolve();
-    });
-  })
-}
+    if (os.type() == "Windows_NT") {
+      let unzip = new adm_zip(`${outputAbsolutePath}/translated.zip`);
+      unzip.extractAllTo(
+        `${outputAbsolutePath}/translated`,
+        /*overwrite*/ true
+      );
+      resolve();
+    } else {
+      const cmd = `unzip ${outputAbsolutePath}/translated.zip -d ${outputAbsolutePath}/translated`;
+      exec(cmd, (err, stdout, stderr) => {
+        if (err) reject(stderr);
+        else resolve();
+      });
+    }
+  });
+};
 
 module.exports = unzip;
