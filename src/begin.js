3ce1ec7f5dc669195a9565f9f6c8efb21f09b0c3
唐佳 <game428@users.noreply.github.com>
2023-05-09 10:11:49 +0800

    first

--- src/begin.js
+++ src/begin.js
@@ -1,149 +1,315 @@
-const path = require('path');
-const fs = require('fs');
-const crowdin = require('@crowdin/crowdin-api-client');
+const path = require("path");
+const fs = require("fs");
+const crowdin = require("@crowdin/crowdin-api-client");
 
-const downloadZip = require('./downloadZip');
-const unzip = require('./unzip');
-const convertJson2Js = require('./convertJson2Js');
-const clearOutput = require('./clearOutput');
+const downloadZip = require("./downloadZip");
+const unzip = require("./unzip");
+const convertJson2Js = require("./convertJson2Js");
+const clearOutput = require("./clearOutput");
+
+async function getFileId(
+  sourceFilesApi,
+  projectId,
+  filePath,
+  branchId = undefined,
+  directoryId = undefined
+) {
+  const listParams = directoryId ? { directoryId } : { branchId };
+  const filesList = await sourceFilesApi.listProjectFiles(
+    projectId,
+    listParams
+  );
+  const file = filesList.data.find((f) => f.data.path === filePath);
+  console.log(file, filesList.data, filePath);
+  return file ? file.data.id : null;
+}
+
+async function getOrCreateDirectory(
+  sourceFilesApi,
+  projectId,
+  branchId,
+  directoryPath
+) {
+  const directoriesRes = await sourceFilesApi.listProjectDirectories(
+    projectId,
+    {
+      branchId,
+    }
+  );
+
+  let directory = directoriesRes.data.find(
+    (dir) => dir.data.name === directoryPath
+  );
+
+  if (!directory) {
+    console.log(`Creating remote directory: ${directoryPath}`);
+    directory = await sourceFilesApi.createDirectory(projectId, {
+      name: directoryPath,
+      branchId,
+    });
+  }
+
+  return directory.data.id;
+}
+
+async function createDirectoriesRecursively(
+  sourceFilesApi,
+  projectId,
+  branchId,
+  directoryPath
+) {
+  const pathParts = directoryPath.split("/");
+  let currentPath = "";
+  let currentDirectoryId;
+
+  for (const pathPart of pathParts) {
+    currentPath = path.join(currentPath, pathPart);
+    currentDirectoryId = await getOrCreateDirectory(
+      sourceFilesApi,
+      projectId,
+      branchId,
+      currentPath
+    );
+  }
+
+  return currentDirectoryId;
+}
 
 module.exports = async (configPath) => {
-  // 获取配置
-  let config;
-  const configAbsolutePath = path.join(path.resolve('./'), configPath);
-  if (fs.existsSync(configAbsolutePath)) {
-    config = require(configAbsolutePath);
-  } else {
-    throw new Error('config file does not exist');
-  }
-  const { token, engineId, projectId, targetLanguageIds, branch, file, sourceFilePath, outputPath, outputFilePrefix, isOutputFullName } = config;
+  try {
+    // 获取配置
+    const configAbsolutePath = path.resolve(configPath);
+    if (!fs.existsSync(configAbsolutePath)) {
+      throw new Error("config file does not exist");
+    }
+    const config = require(configAbsolutePath);
 
-  // 获取配置文件所在路径
-  const prefixPath = path.dirname(configAbsolutePath);
+    const {
+      token,
+      engineId,
+      projectId,
+      targetLanguageIds,
+      branch,
+      file,
+      sourceFilePath,
+      outputPath,
+      outputFilePrefix,
+      isOutputFullName,
+      directory,
+    } = config;
 
-  // 检查语言包源文件是否存在
-  const sourceFileAbsolutePath = path.join(prefixPath, sourceFilePath);
-  if (!fs.existsSync(sourceFileAbsolutePath)) {
-    throw new Error('source lang file dose not exist');
-  }
+    // 获取配置文件所在路径
+    const prefixPath = path.dirname(configAbsolutePath);
 
-  // 检查输出文件目录是否存在
-  const outputAbsolutePath = path.join(prefixPath, outputPath);
-  if (!fs.existsSync(outputAbsolutePath)) {
-    fs.mkdirSync(outputAbsolutePath);
-  }
+    // 检查语言包源文件是否存在
+    const sourceFileAbsolutePath = path.join(prefixPath, sourceFilePath);
+    if (!fs.existsSync(sourceFileAbsolutePath)) {
+      throw new Error("source lang file does not exist");
+    }
 
-  // 创建crowdin sdk实例
-  const { translationsApi, sourceFilesApi, uploadStorageApi } = new crowdin.default({ token });
+    // 检查输出文件目录是否存在
+    const outputAbsolutePath = path.join(prefixPath, outputPath);
+    if (!fs.existsSync(outputAbsolutePath)) {
+      fs.mkdirSync(outputAbsolutePath);
+    }
 
-  // 获取分支id和文件id
-  let branchId, fileId;
+    // 创建crowdin sdk实例
+    const { translationsApi, sourceFilesApi, uploadStorageApi } =
+      new crowdin.default({ token });
 
-  // 检查是否创建远程分支
-  console.log('check if a individual branch has been created...')
-  const branchListRes = await sourceFilesApi.listProjectBranches(projectId, { name: branch.name });
-  if (branchListRes.data.length > 0) {
-    branchId = branchListRes.data[0].data.id;
+    // 获取分支id和文件id
+    let branchId, fileId;
 
-    // fetch fileId
-    console.log('fetch remote fileId of individual branch...')
-    const listFileRes = await sourceFilesApi.listProjectFiles(projectId, { branchId });
-    fileId = listFileRes.data[0].data.id;
-
-    // create storage
-    console.log('create a storage for remote file...')
-    const sourceFile = fs.readFileSync(sourceFileAbsolutePath);
-    const sourceStr = sourceFile.toString();
-    const begin = sourceStr.indexOf('{');
-    const end = sourceStr.lastIndexOf('}') + 1;
-    const jsonStr = sourceStr.substring(begin, end);
-    const lang = eval('(' + jsonStr + ')'); // JSON.parse无法解析不带双引号的json字符串，eval可以转换
-    const addStorageRes = await uploadStorageApi.addStorage(file.name, lang);
-    const storageId = addStorageRes.data.id;
-
-    // update file
-    console.log('update remote file...')
-    await sourceFilesApi.updateOrRestoreFile(projectId, fileId, { storageId });
-  } else {
-    // create branch
-    console.log('create individual branch...')
-    const createBranchRes = await sourceFilesApi.createBranch(projectId, { name: branch.name, title: branch.title });
-    branchId = createBranchRes.data.id;
-
-    // create storage
-    console.log('create a storage for remote file...')
-    const sourceFile = fs.readFileSync(sourceFileAbsolutePath);
-    const sourceStr = sourceFile.toString();
-    const begin = sourceStr.indexOf('{');
-    const end = sourceStr.lastIndexOf('}') + 1;
-    const jsonStr = sourceStr.substring(begin, end);
-    const lang = eval('(' + jsonStr + ')'); // JSON.parse无法解析不带双引号的json字符串，eval可以转换
-    const addStorageRes = await uploadStorageApi.addStorage(file.name, lang);
-    const storageId = addStorageRes.data.id;
-
-    // create file
-    console.log('create remote file');
-    const createFileRes = await sourceFilesApi.createFile(projectId, { 
-      branchId,
-      name: file.name,
-      title: file.title,
-      storageId: storageId,
-      type: 'json'
+    // 检查是否创建远程分支
+    console.log("check if a individual branch has been created...");
+    const branchListRes = await sourceFilesApi.listProjectBranches(projectId, {
+      name: branch.name,
     });
-    fileId = createFileRes.data.id;
+
+    if (branchListRes.data.length > 0) {
+      branchId = branchListRes.data[0].data.id;
+    } else {
+      // create branch
+      console.log("create individual branch...");
+      const createBranchRes = await sourceFilesApi.createBranch(projectId, {
+        name: branch.name,
+        title: branch.title,
+      });
+      branchId = createBranchRes.data.id;
+    }
+
+    // 创建或获取目录
+    const directoryId = await createDirectoriesRecursively(
+      sourceFilesApi,
+      projectId,
+      branchId,
+      directory
+    );
+
+    // 获取或创建文件
+    fileId = await getFileId(
+      sourceFilesApi,
+      projectId,
+      `/${branch.name}/${directory}/${file.name}`,
+      branchId,
+      directoryId
+    );
+
+    console.log("getFileId:", fileId);
+    // 读取文件
+    const sourceFile = fs.readFileSync(sourceFileAbsolutePath);
+    const sourceStr = sourceFile.toString();
+    const begin = sourceStr.indexOf("{");
+    const end = sourceStr.lastIndexOf("}") + 1;
+    const jsonStr = sourceStr.substring(begin, end);
+    const lang = eval("(" + jsonStr + ")"); // JSON.parse无法解析不带双引号的json字符串，eval可以转换
+    const addStorageRes = await uploadStorageApi.addStorage(file.name, lang);
+    const storageId = addStorageRes.data.id;
+
+    if (fileId === null) {
+      // 创建文件
+      console.log("create remote file");
+      const createFileRes = await sourceFilesApi.createFile(projectId, {
+        branchId,
+        directoryId,
+        name: file.name,
+        title: file.title,
+        storageId: storageId,
+        type: "json",
+      });
+      fileId = createFileRes.data.id;
+    } else {
+      // 更新文件
+      console.log("update remote file...");
+      await sourceFilesApi.updateOrRestoreFile(projectId, fileId, {
+        storageId,
+      });
+    }
+
+    // 预翻译
+    console.log("start translating...");
+    let {
+      data: { identifier: preTransId, progress: preTransProgress },
+    } = await translationsApi.applyPreTranslation(projectId, {
+      languageIds: targetLanguageIds,
+      fileIds: [fileId],
+      method: "mt",
+      engineId: engineId,
+    });
+
+    // 循环检查预翻译状态
+    while (preTransProgress < 100) {
+      const preTransStatusRes = await translationsApi.preTranslationStatus(
+        projectId,
+        preTransId
+      );
+      preTransProgress = preTransStatusRes.data.progress;
+      console.log(`pre-translation progress: ${preTransProgress}%`);
+    }
+
+    // 构建
+    console.log("start building...");
+    let {
+      data: { id: buildId, progress: buildProgress },
+    } = await translationsApi.buildProject(projectId, {
+      branchId,
+      targetLanguageIds: targetLanguageIds,
+    });
+
+    // 循环检查构建状态
+    while (buildProgress < 100) {
+      const buildStatusRes = await translationsApi.checkBuildStatus(
+        projectId,
+        buildId
+      );
+      buildProgress = buildStatusRes.data.progress;
+      console.log(`building progress: ${buildProgress}%`);
+    }
+
+    // 获取翻译文件的URL
+    console.log("fetch url of translated zip...");
+    const {
+      data: { url },
+    } = await translationsApi.downloadTranslations(projectId, buildId);
+
+    // 下载翻译文件
+    console.log("download translated zip...");
+    await downloadZip(url, outputAbsolutePath);
+
+    // 解压翻译文件
+    console.log("unzip...");
+    await unzip(outputAbsolutePath);
+
+    // 将翻译文件从JSON转换为JS
+    console.log("convert json to js...");
+    await convertJson2Js(
+      outputAbsolutePath,
+      outputFilePrefix,
+      targetLanguageIds,
+      isOutputFullName
+    );
+
+    // 清理输出文件夹
+    console.log("clear download file...");
+    clearOutput(outputAbsolutePath);
+
+    console.log("done.");
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+};
+
+// 优化后的代码模块
+async function createDirectoriesRecursively(
+  sourceFilesApi,
+  projectId,
+  branchId,
+  directoryPath
+) {
+  const pathParts = directoryPath.split("/");
+  let currentPath = "";
+  let parentDirectoryId;
+
+  for (const pathPart of pathParts) {
+    currentPath = path.join(currentPath, pathPart);
+    console.log("branchId:", branchId, "parentDirectoryId:", parentDirectoryId);
+
+    const listParams = parentDirectoryId
+      ? { directoryId: parentDirectoryId }
+      : { branchId };
+
+    const directoriesList = await sourceFilesApi.listProjectDirectories(
+      projectId,
+      listParams
+    );
+
+    const existingDirectory = directoriesList.data.find(
+      (dir) => dir.data.name === pathPart
+    );
+
+    if (existingDirectory) {
+      parentDirectoryId = existingDirectory.data.id;
+    } else {
+      console.log(`Creating remote directory: ${currentPath}`);
+
+      const directoryData = {
+        name: pathPart,
+        title: pathPart,
+      };
+
+      if (parentDirectoryId) {
+        directoryData.directoryId = parentDirectoryId;
+      } else {
+        directoryData.branchId = branchId;
+      }
+
+      const newDirectory = await sourceFilesApi.createDirectory(
+        projectId,
+        directoryData
+      );
+      parentDirectoryId = newDirectory.data.id;
+    }
   }
 
-  // pre-translation
-  console.log('start translating...')
-  let { data: { identifier: preTransId, progress: preTransProgress } } = await translationsApi.applyPreTranslation(projectId, {
-    "languageIds": targetLanguageIds,
-    "fileIds": [
-      fileId
-    ],
-    "method": "mt",
-    "engineId": engineId,
-  })
-
-  // loop to check the status of pre-translation
-  while (preTransProgress < 100) {
-    const preTransStatusRes = await translationsApi.preTranslationStatus(projectId, preTransId);
-    preTransProgress = preTransStatusRes.data.progress;
-    console.log(`pre-translation progress: ${preTransProgress}%`)
-  }
-
-  // build
-  console.log('start building...')
-  let { data: { id: buildId, progress: buildProgress } } = await translationsApi.buildProject(projectId, {
-    branchId,
-    "targetLanguageIds": targetLanguageIds
-  }) 
-           
-  // loop to check the status of building
-  while (buildProgress < 100) {
-    let buildStatusRes = await translationsApi.checkBuildStatus(projectId, buildId);
-    buildProgress = buildStatusRes.data.progress;
-    console.log(`building progress: ${buildProgress}%`)
-  }
-
-  // fetch translations url
-  console.log('fetch url of translated zip...')
-  const { data: { url } } = await translationsApi.downloadTranslations(projectId, buildId);
-
-  // download translations
-  console.log('download translated zip...');
-  await downloadZip(url, outputAbsolutePath);
-
-  // unzip translations
-  console.log('unzip...');
-  await unzip(outputAbsolutePath);
-
-  // convert translations
-  console.log('convert json to js...');
-  await convertJson2Js(outputAbsolutePath, outputFilePrefix, targetLanguageIds, isOutputFullName);
-  
-  // clear output folder
-  console.log('clear download file...');
-  clearOutput(outputAbsolutePath);
-
-  console.log('done.');
+  return parentDirectoryId;
 }
